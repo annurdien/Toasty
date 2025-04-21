ef565886b677b243fbd629591f09b14ce77fd6cb
Annurdien Rasyid <annurdien.dev@gmail.com>
2025-04-22 00:21:25 +0700

    chore: update Swift tools version to 5.7 and format Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,25 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Toasty",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v10_15),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Toasty",
             targets: ["Toasty"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Toasty"),
+            name: "Toasty",
+            dependencies: []),
         .testTarget(
             name: "ToastyTests",
-            dependencies: ["Toasty"]
-        ),
+            dependencies: ["Toasty"]),
     ]
 )
